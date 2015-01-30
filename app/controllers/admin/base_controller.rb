0d9e9360f43655d9d78bee554f59ac65b7446569
Wal33D <aquataze@yahoo.com>
2015-01-29 22:19:55 -0500

    configured admin url

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,8 +2,9 @@ class Admin::BaseController < ApplicationController
 
   before_filter :require_admin
   layout "admin"
-
+#when we navigate to /admin we will hit the index method, this will redirect us to the admin users_path, This is temporary at some point we will delete it and use index as a dashboard page 
   def index
+  redirect_to admin_users_path
   end
 
   protected
