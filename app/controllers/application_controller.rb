ebf9cb2d3a7a35c36778227b15cb561e5a4aab78
Wal33D <aquataze@yahoo.com>
2015-02-03 04:23:51 -0500

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) << :avatar << :name
+    devise_parameter_sanitizer.for(:sign_up) << :name
+
   end
   
   def render404
