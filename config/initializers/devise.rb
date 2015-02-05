ab168ff4dc3172a96b17c265379a221436a50591
Wal33D <aquataze@yahoo.com>
2015-02-05 02:56:01 -0500

    .

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -169,9 +169,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   #FB PRODUCTION APP
-  config.omniauth :facebook, '430782430407283', '231adb2f5939c05c2a8d5d13b83f9f67'
+  #config.omniauth :facebook, '430782430407283', '231adb2f5939c05c2a8d5d13b83f9f67'
   #FB TEST APP
-  #config.omniauth :facebook, '450488161770043', '9cf1b758cf5842a71e7bb9f8223defee'
+  config.omniauth :facebook, '450488161770043', '9cf1b758cf5842a71e7bb9f8223defee'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
