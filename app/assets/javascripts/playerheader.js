3b47865e835dba413f6d0fb94c841fc8e6d9df35
Wal33D <aquataze@yahoo.com>
2015-03-21 04:58:45 -0400

    .

--- app/assets/javascripts/playerheader.js
+++ app/assets/javascripts/playerheader.js
@@ -80,6 +80,10 @@ function jqUpdateSize() {
     // $('.ZombiePane' ).css('height', height/2.1 -headerheight);  
     $('#ZombieListButton, #PlayerListButton').css('height', height / 14 );
     $('#ZombieListButton, #PlayerListButton, h3.jumbotron').css('font-size', height / 43);
+    $('.item').css('height', height / 1.8);
+    $('.bigframe-body').css('height', height / 1.2);
+
+     //   $('.item').css('max-height', height/3);
 
     $('#mapContainer').css('max-height', height - searchheight - headerheight - (1 / 2) * searchheight);
 
