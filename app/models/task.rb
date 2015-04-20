c7c9b629b8210725a330df6b9348ec11bee37d26
Wal33D <aquataze@yahoo.com>
2015-04-20 02:27:14 -0400

    .

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ActiveRecord::Base; 
-	validate :check_tagger_not_taggee
-	  validates_uniqueness_of :description
-validates :description, :presence => true
+	validates_uniqueness_of :description
+	validates :description, :presence => true
 
 
 def check_tagger_not_taggee
