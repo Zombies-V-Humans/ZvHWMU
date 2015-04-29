0f1423c548a44a6e521d9e7c79d80e3a66755de6
Wal33D <aquataze@yahoo.com>
2015-04-29 00:09:45 -0400

    .

--- app/assets/javascripts/playerheader.js
+++ app/assets/javascripts/playerheader.js
@@ -75,10 +75,10 @@ function setHeight(element, attr, divisor) {
 }
 
 function jqUpdateSize() {
-    setHeight('body, .container-fluid', 'max-height', 1.2);
-    setHeight('#ZombieListButton, #PlayerListButton, h3.jumbotron, #user-name', 'font-size', 43);
+    setHeight('body, .container-fluid', 'max-height', 1);
+    setHeight('#ZombieListButton, #PlayerListButton, h3.jumbotron, #user-name', 'font-size', 44.5);
     setHeight('.user-names', 'font-size', 43);
-    setHeight('#ZombieListButton, #PlayerListButton', 'height', 17);
+    setHeight('#ZombieListButton, #PlayerListButton', 'height', 17.5);
     setHeight('.item', 'height', 2.4);
     setHeight('.submitbox, .box', 'height', 16);
     setHeight('bigframe-body', 'height', 1.22);
