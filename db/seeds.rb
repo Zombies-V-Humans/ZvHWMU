9c975648b35b2af5d12cae4832929da856109f1f
Wal33D <aquataze@yahoo.com>
2015-04-28 22:27:43 -0400

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,35 @@
-User.create!(:name => "Admin",
-             :email => "admin@zvh.edu", 
+User.create!(:name => "Admin1",
+             :email => "zvhwmich@gmail.com", 
          		 :password =>"password", 
          		 :created_at => 2.days.ago,
              :last_sign_in_at => 1.day.ago,
              :updated_at => 5.hours.ago).make_admin
 
-12.times do |n|
+User.create!(:name => "Admin2",
+             :email => "zvhwmich2@gmail.com", 
+                 :password =>"password", 
+                 :created_at => 2.days.ago,
+             :last_sign_in_at => 1.day.ago,
+             :updated_at => 5.hours.ago).make_admin
+
+User.create!(:name => "Admin3",
+             :email => "zvhwmich3@gmail.com", 
+                 :password =>"password", 
+                 :created_at => 2.days.ago,
+             :last_sign_in_at => 1.day.ago,
+             :updated_at => 5.hours.ago).make_admin
+
+120.times do |n|
  name = Faker::Name.name 
  email = Faker::Internet.email
- avatar = Faker::Avatar.image( "50x50")
+ avatar = Faker::Avatar.image( "#{n*2}x#{n*3}")
  password = "password"
 User.create!(:name => name,
              :email => email, 
              :avatar => avatar,
-         		 :password => password, 
-         		 :created_at => 2.days.ago,
+                 :password => password, 
+                 :created_at => 2.days.ago,
              :last_sign_in_at => 1.day.ago,
              :updated_at => 5.hours.ago)
-	end
+    end
 
